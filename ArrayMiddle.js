7b7b2ced53423a621ac041d7d28f913a0b3d46aa
Daniel Kim <Danielhungkim@gmail.com>
2016-04-27 10:55:19 -0700

    Create ArrayMiddle.js

---
+++ ArrayMiddle.js
@@ -0,0 +1,4 @@
+//Given an array of negative/positive integers, return the element in the center position of the array.
+
+//If the array has an even number of elements, return the average of the two middle elements instead.
+
