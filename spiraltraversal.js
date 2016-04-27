0b4e96b47cf9a3753a1d681406357132801112ee
Daniel Kim <Danielhungkim@gmail.com>
2016-04-26 19:39:31 -0700

    Update spiraltraversal.js

--- spiraltraversal.js
+++ spiraltraversal.js
@@ -1,3 +1,6 @@
+// Write a function that accepts a 2-dimensional array (that is, an array containing many same-length arrays),
+// and prints out every value found, but in a spiral from the upper left in to the center.
+
 function spiralTraversal (matrix) {
   var temp = [];
   var clone = matrix.slice();
