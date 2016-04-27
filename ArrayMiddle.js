a952d2fa6e595b07f0d5fc1d54937ad339f0bb58
Daniel Kim <Danielhungkim@gmail.com>
2016-04-27 10:55:33 -0700

    Update ArrayMiddle.js

--- ArrayMiddle.js
+++ ArrayMiddle.js
@@ -2,3 +2,10 @@
 
 //If the array has an even number of elements, return the average of the two middle elements instead.
 
+function middle (numbers) {
+  if(numbers.length % 2 !== 0) {
+    return numbers[Math.floor(numbers.length/2)];
+  }else {
+    return (numbers[numbers.length/2] + numbers[numbers.length/2 - 1]) / 2;
+  }
+}
