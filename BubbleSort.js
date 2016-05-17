fd461b682a040ac1a94d3bed6527907d5aa158e3
Daniel Kim <Danielhungkim@gmail.com>
2016-05-17 14:50:34 -0700

    Update BubbleSort.js

--- BubbleSort.js
+++ BubbleSort.js
@@ -6,3 +6,19 @@
 //In this way, the largest values ‘bubble’ to the end of the array.
 //Once it gets to the end of the array, it starts over and repeats the process until the array is sorted numerically.
 //Implement a function that takes an array and sorts it using this technique.
+
+
+function bubbleSort (input) {
+  var array = input;
+  var holder;
+  for(var i = 0; i < input.length; i++){
+    for(var j = 0; j < input.length - 1; j++){
+      if(array[j] > array[j+1]){
+        holder = array[j+1];
+        array[j+1] = array[j];
+        array[j] = holder;
+      }
+    }
+  }
+  return array;
+ }
