b8603cc3f16658e5660c55f24f45667492dcd4a9
Daniel Kim <Danielhungkim@gmail.com>
2016-05-24 13:30:48 -0700

    Update InvertBinaryTree.js

--- InvertBinaryTree.js
+++ InvertBinaryTree.js
@@ -11,3 +11,24 @@
 //   7     2
 //  / \   / \
 // 9   6 3   1
+
+var invertTree = function(root) {
+    if(root === null){
+        return null;
+    }
+    function recurse(node) {
+        var temp = node.left;
+        node.left = node.right;
+        node.right = temp;
+        
+        if(node.left !== null){
+            recurse(node.left);
+        }
+        if(node.right !== null){
+            recurse(node.right);
+        }
+    }
+    
+    recurse(root);
+    return root;
+};
