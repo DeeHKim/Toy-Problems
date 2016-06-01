278c1e10fc8de80871a8d307fd776c3697051603
Daniel Kim <Danielhungkim@gmail.com>
2016-06-01 14:56:37 -0700

    Create FunctionBind.js

---
+++ FunctionBind.js
@@ -0,0 +1,3 @@
+//Implement the function ‘bind’, which accepts a function and a context as arguments. //
+//The context argument should override an existing context that the function is defined in. 
+//Your bind function should return the passed in function.
