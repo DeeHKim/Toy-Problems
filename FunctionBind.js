dfafa09f5257fa67e54431a54e7a1f8c9941785b
Daniel Kim <Danielhungkim@gmail.com>
2016-06-01 14:57:00 -0700

    Update FunctionBind.js

--- FunctionBind.js
+++ FunctionBind.js
@@ -1,3 +1,22 @@
 //Implement the function ‘bind’, which accepts a function and a context as arguments. //
 //The context argument should override an existing context that the function is defined in. 
 //Your bind function should return the passed in function.
+
+
+var bind = function(func, context){
+ var args = Array.prototype.slice.call(arguments, 2);
+ var fn = func;
+  return function(){
+   args2 = args.concat(Array.prototype.slice.call(arguments));
+     return fn.apply(context, args2);
+  };
+}; 
+
+Function.prototype.bind = function(context) {
+    var array1 = Array.prototype.slice.call(arguments, 1);
+    var func = this;
+  return function(){
+     args2 = array1.concat(Array.prototype.slice.call(arguments));
+     return func.apply(context, args2);
+  };
+};
