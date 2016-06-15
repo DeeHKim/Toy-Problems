ce27e0ed6e78fbd3d36f45dac49a0f42897af5fc
Daniel Kim <Danielhungkim@gmail.com>
2016-06-15 12:34:21 -0700

    Update AllAnagrams.js

--- AllAnagrams.js
+++ AllAnagrams.js
@@ -1,5 +1,5 @@
 //Given a single input string, write a function that produces all
-possible anagrams of a string and outputs them as an array.
+//possible anagrams of a string and outputs them as an array.
 
 function allAnagrams (string) {
   var result = [];
